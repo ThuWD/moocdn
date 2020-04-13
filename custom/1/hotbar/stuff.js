53c8fcedf6625b96d0b6f1d1f9e1b111f4c9f98e
ThuWD <61614127+ThuWD@users.noreply.github.com>
2020-04-13 18:59:25 +1000

    Update stuff.js

--- custom/1/hotbar/stuff.js
+++ custom/1/hotbar/stuff.js
@@ -1,1 +1,1 @@
-document.getElementById("actionBarItem0").style.backgroundImage = "url('https://i.imgur.com/K15SffX.png')";
+document.getElementById("actionBarItem0").style.backgroundImage = "url('https://i.imgur.com/ygN5UFA.png')";
