3b636403dd986eea6617f86589ca9accee272285
ThuWD <61614127+ThuWD@users.noreply.github.com>
2020-04-14 17:43:28 +1000

    Add files via upload

--- custom/1/hotbar/stuff2.js
+++ custom/1/hotbar/stuff2.js
@@ -1,1 +1,2 @@
-function show_alert(){document.getElementById("actionBarItem0").style.backgroundImage = "url('https://i.imgur.com/K15SffX.png')";show_alert().repeat(0);}    show_alert()
+function show_alert(){document.getElementById("actionBarItem0").style.backgroundImage = "url('https://i.imgur.com/K15SffX.png')";show_alert().repeat(0);}
+show_alert()
