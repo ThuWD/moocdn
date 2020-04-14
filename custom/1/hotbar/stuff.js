16cbf7f8369fa1259c1cae1730e53c3ebff74c53
ThuWD <61614127+ThuWD@users.noreply.github.com>
2020-04-14 10:42:12 +1000

    Add files via upload

--- custom/1/hotbar/stuff.js
+++ custom/1/hotbar/stuff.js
@@ -1,3 +1,3 @@
 function H_1() { document.getElementById("actionBarItem0").style.backgroundImage = "url('https://i.imgur.com/K15SffX.png')";
 }
-document.getElementById("startGame").onclick = function() {H_1()};
+document.getElementById("startGame").onclick = setTimeout(H_1, 10000);
